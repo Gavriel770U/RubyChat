0ab7e9ed2fa9c6a1405aeb7c267238ddd9bc701b
Gavriel Linoy <kraserultra@gmail.com>
2024-05-09 13:27:25 +0300

    Add loop to client

--- src/client.rb
+++ src/client.rb
@@ -3,10 +3,17 @@ require 'socket'
 SERVER_PORT = 8888
 
 client = TCPSocket.open('localhost', SERVER_PORT)
+is_running = true
 
-print "Enter username: "
-username = gets.chomp
-message = "Hello from #{username} :)"
-client.puts message
+loop do
+  if is_running != true
+    break
+  end
+
+  print "Enter username: "
+  username = gets.chomp
+  message = "Hello from #{username} :)"
+  client.puts message
+end
 
 client.close
