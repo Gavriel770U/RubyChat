05b12522c899d775a569e5514f61b77cd3ea596a
Gavriel Linoy <kraserultra@gmail.com>
2024-05-10 13:14:10 +0300

    Resize window and add entr

--- src/gui.rb
+++ src/gui.rb
@@ -1,8 +1,12 @@
 require 'tk'
 
-root = TkRoot.new {
-   title "RubyChat"
-   geometry "800x600"
-}
+root = TkRoot.new(title: "RubyChat", geometry: "600x400")
+
+message_entry = TkEntry.new(root)
+message_variable = TkVariable.new
+message_entry.textvariable = message_variable
+message_entry.place('height' => 25, 'width' => 300, 'x' => 50, 'y' => 300)
+
+
 
 Tk.mainloop
