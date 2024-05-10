59a7a11150d69ddc8f9ba16e0726fc97ec799cc4
Gavriel Linoy <kraserultra@gmail.com>
2024-05-10 13:29:29 +0300

    Basic button callback

--- src/gui.rb
+++ src/gui.rb
@@ -1,20 +1,26 @@
 require 'tk'
 
+def send_message
+   puts $message_entry.value
+   $message_entry.value = ""
+end
+
 root = TkRoot.new(title: "RubyChat", geometry: "600x400")
 
-message_entry = TkEntry.new(root)
-message_variable = TkVariable.new
-message_entry.textvariable = message_variable
-message_entry.place('height' => 25, 'width' => 300, 'x' => 50, 'y' => 300)
+$message_entry = TkEntry.new(root)
+$message_variable = TkVariable.new
+$message_entry.textvariable = $message_variable
+$message_entry.place('height' => 25, 'width' => 300, 'x' => 50, 'y' => 300)
 
-send_message_button = TkButton.new(root) do
+$send_message_button = TkButton.new(root) do
    text ">"
    borderwidth 2
    state "normal"
    foreground "red"
    activebackground "red"
+   command (proc {send_message})
 end
 
-send_message_button.place('height' => 25, 'width' => 25, 'x' => 380, 'y' => 300)
+$send_message_button.place('height' => 25, 'width' => 25, 'x' => 380, 'y' => 300)
 
 Tk.mainloop
