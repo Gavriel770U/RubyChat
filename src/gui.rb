d50b951b75ad64599ecba7a5a1d164b3109e8d96
Gavriel Linoy <kraserultra@gmail.com>
2024-05-10 13:20:16 +0300

    Add basic button

--- src/gui.rb
+++ src/gui.rb
@@ -7,6 +7,14 @@ message_variable = TkVariable.new
 message_entry.textvariable = message_variable
 message_entry.place('height' => 25, 'width' => 300, 'x' => 50, 'y' => 300)
 
+send_message_button = TkButton.new(root) do
+   text ">"
+   borderwidth 2
+   state "normal"
+   foreground "red"
+   activebackground "red"
+end
 
+send_message_button.place('height' => 25, 'width' => 25, 'x' => 380, 'y' => 300)
 
 Tk.mainloop
