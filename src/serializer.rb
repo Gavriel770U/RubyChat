29b156145674ee35f7ef95d0a98e7c448f717c92
Gavriel Linoy <kraserultra@gmail.com>
2024-05-13 01:46:53 +0300

    Add skeletons for static functions of serializer

--- src/serializer.rb
+++ src/serializer.rb
@@ -7,4 +7,18 @@ class Serializer
   def initialize
 
   end
+
+  def self.serialize_login_request
+
+  end
+
+  def self.serialize_send_message_request
+
+  end
+
+  def self.serialize_logout_request
+
+  end
+
+
 end
