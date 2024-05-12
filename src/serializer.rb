92ecaae8c7f289c90d9c0aeca66a6809bd77e567
Gavriel Linoy <kraserultra@gmail.com>
2024-05-13 01:41:18 +0300

    Add requests.rb and skeleton for serializer

---
+++ src/serializer.rb
@@ -0,0 +1,10 @@
+require 'json'
+
+# request = '{"status": 100, "username": "Gavriel"}'
+# j = JSON.parse(request) # can access j['status'], j['username']
+
+class Serializer
+  def initialize
+
+  end
+end
