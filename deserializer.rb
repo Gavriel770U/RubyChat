c0988c1f693b994120b7649f8e338c0512c9a696
Gavriel Linoy <kraserultra@gmail.com>
2024-05-14 09:55:41 +0300

    Implement deserialize_login_request function

--- deserializer.rb
+++ deserializer.rb
@@ -9,7 +9,10 @@ class Deserializer
   end
 
   def self.deserialize_login_request (bytes_data)
-
+    length = bytes_data[1..8].pack('C*').unpack('Q').first
+    json = JSON.parse([9..9+length-1].pack('C*'))
+    lr = LoginRequest.new(json['status'], json['username'])
+    return lr
   end
 
   def self.deserialize_send_message_request (bytes_data)
