5c366279efd775adae021e82a3f33e9712061b20
Gavriel Linoy <kraserultra@gmail.com>
2024-05-14 13:05:55 +0300

    Add LoggedUsers array and check if user exists there, if true dont log, also close socket after running loop, causes error currently

--- src/logged_user.rb
+++ src/logged_user.rb
@@ -2,4 +2,8 @@ class LoggedUser
   def initialize (username)
     @username = username
   end
+
+  def username
+    return @username
+  end
 end
--- src/server.rb
+++ src/server.rb
@@ -2,10 +2,22 @@ require 'socket'
 require_relative 'responses'
 require_relative 'serializer'
 require_relative 'deserializer'
+require_relative 'logged_user'
 
 SERVER_PORT = 8888
 
 server = TCPServer.new('localhost', SERVER_PORT)
+logged_users = Array.new
+
+def find_logged_user(target_logged_user, logged_users)
+  logged_users.each do |logged_user|
+    if logged_user.username == target_logged_user.username
+      return true
+    end
+  end
+
+  return false
+end
 
 puts "Running server..."
 
@@ -24,13 +36,21 @@ loop do
       login_request = Deserializer.deserialize_login_request(bytes_data)
       puts login_request
 
-      login_reponse = LoginResponse.new(ResponseCode::LOGIN_SUCCESS)
+      logged_user = LoggedUser.new(login_request['username'])
+      user_exists = find_logged_user(logged_user, logged_users)
+      if !user_exists
+        login_reponse = LoginResponse.new(ResponseCode::LOGIN_SUCCESS)
+        logged_users.push(logged_user)
+      else
+        login_reponse = LoginResponse.new(ResponseCode::LOGIN_FAILURE)
+      end
+
       bytes_data = Serializer.serialize_login_response(login_reponse)
       client.send(bytes_data.pack('C*'), 0)
-
-      client.close
-      Thread.exit
     end
+
+    client.close
+    Thread.exit
   end
 end
 
