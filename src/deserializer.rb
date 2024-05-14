907fee22daf15e13563be25b05ff193d16c991c2
Gavriel Linoy <kraserultra@gmail.com>
2024-05-14 10:17:51 +0300

    Implement deserialize_send_message_response function

--- src/deserializer.rb
+++ src/deserializer.rb
@@ -44,7 +44,11 @@ class Deserializer
 
 
   def self.deserialize_send_message_response (bytes_data)
+    length = bytes_data[1..8].pack('C*').unpack('Q').first
+    json = JSON.parse(bytes_data[9..9+length-1].pack('C*'))
+    smr = SendMessageResponse.new(json['status'])
 
+    return smr
   end
 
   def self.deserialize_logout_response (bytes_data)
