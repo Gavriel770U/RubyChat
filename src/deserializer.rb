64f4f09a90533c062c07f9b5700a5f42e96b42eb
Gavriel Linoy <kraserultra@gmail.com>
2024-05-19 14:10:41 +0300

    Implement deserialize_refresh_request function

--- src/deserializer.rb
+++ src/deserializer.rb
@@ -26,6 +26,14 @@ class Deserializer
     return smr
   end
 
+  def self.deserialize_refresh_request (bytes_data)
+    length = bytes_data[1..8].pack('C*').unpack('Q').first
+    json = JSON.parse(bytes_data[9..9+length-1].pack('C*'))
+    rr = RefreshRequest.new(json['status'])
+
+    return rr
+  end
+
   def self.deserialize_logout_request (bytes_data)
     length = bytes_data[1..8].pack('C*').unpack('Q').first
     json = JSON.parse(bytes_data[9..9+length-1].pack('C*'))
